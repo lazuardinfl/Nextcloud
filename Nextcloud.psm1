c11a8b1f0dbb37145620eeae2c5058ee7439e351
Lazuardi Naufal <lazuardi.n@samsung.com>
2024-08-05 13:14:10 +0700

    add upload file to nextcloud

--- Nextcloud.psm1
+++ Nextcloud.psm1
@@ -22,3 +22,27 @@ function Get-NextcloudItem {
     }
     catch { if ($silent) { return $null } else { throw } }
 }
+
+function Set-NextcloudItem {
+    [OutputType([bool])]
+    param (
+        [Alias("NextcloudUrl")] [ValidateNotNullOrWhiteSpace()] [string]$url,
+        [Alias("ItemPath")] [ValidateNotNullOrWhiteSpace()] [string]$path,
+        [Alias("UploadPath")] [ValidateNotNullOrWhiteSpace()] [string]$source,
+        [Alias("UserId")] [ValidateNotNullOrWhiteSpace()] [string]$id,
+        [Alias("UserPassword")] [ValidateNotNullOrWhiteSpace()] [string]$pass,
+        [Alias("OnErrorContinue")] [switch]$silent
+    )
+    try {
+        $rest = @{
+            Uri = "$($url)/remote.php/dav/files/$($id)/$($path)"
+            Method = "Put"
+            Authentication = "Basic"
+            Credential = [pscredential]::new($id, (ConvertTo-SecureString $pass -AsPlainText -Force))
+            InFile = $source
+        }
+        Invoke-RestMethod @rest | Out-Null
+        return $true
+    }
+    catch { if ($silent) { return $false } else { throw } }
+}
